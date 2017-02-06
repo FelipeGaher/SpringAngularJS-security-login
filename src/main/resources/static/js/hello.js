dd144b495fb741acf0b25b6a41293e0588e5f80b
Felipe Gajardo <felipe.gajardo.contractor@bbva.com>
2017-02-06 17:07:03 -0300

    XHR added ~Felipe

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,7 @@
 angular.module('hello', [])
-  .controller('home', function($scope) {
-    $scope.greeting = {id: '899511', content: 'Hello World!'}
+  .controller('home', function($http) {
+	  var self = this;
+	  $http.get('/resource/').then(function(response) {
+	    self.greeting = response.data;
+	  })
 })
