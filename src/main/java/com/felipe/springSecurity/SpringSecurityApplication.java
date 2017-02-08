3e4ef8796c3b35370c230fa2df224c134ae8fbe2
Felipe Gajardo <felipe.gajardo.contractor@bbva.com>
2017-02-08 14:14:09 -0300

    New feature ~Felipe

--- src/main/java/com/felipe/springSecurity/SpringSecurityApplication.java
+++ src/main/java/com/felipe/springSecurity/SpringSecurityApplication.java
@@ -1,11 +1,18 @@
 package com.felipe.springSecurity;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +20,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringSecurityApplication {
 
-	  @RequestMapping("/resource")
-	  public Map<String,Object> home() {
-	    Map<String,Object> model = new HashMap<String,Object>();
-	    model.put("id", UUID.randomUUID().toString());
-	    model.put("content", "Hello World");
-	    return model;
-	  }
-	
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+	/**
+	 * Principal from Spring Security
+	 * 
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	/**
+	 * Handling the Login Request on the Server
+	 * @author fgajardo
+	 *
+	 */
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/")
+					.permitAll().anyRequest().authenticated().and()
+				      .csrf()
+				        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());;
+		}
 	}
 }
